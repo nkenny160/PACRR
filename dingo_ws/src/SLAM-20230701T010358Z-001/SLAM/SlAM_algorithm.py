de73609e9832300264b0601c2365a667842c56a8
DMMagilient <aohare@scu.edu>
2023-07-02 13:01:14 -0700

    Starting to sync datastructure to gridmap

--- dingo_ws/src/SLAM-20230701T010358Z-001/SLAM/SlAM_algorithm.py
+++ dingo_ws/src/SLAM-20230701T010358Z-001/SLAM/SlAM_algorithm.py
@@ -1,5 +1,6 @@
 import numpy
 import pygame
+import ctypes
 
 #SlAM algorithm
 
@@ -40,10 +41,21 @@ class map:
             for j in range(divisions):
                  pygame.draw.line(map._VARS['surf'], map.BLACK, (0 + map.LEFTRIGHTPAD, 0 + map.TOPBOTPAD + (j * cell_size_vert)), (map.WIDTH - map.LEFTRIGHTPAD, 0 + map.TOPBOTPAD + (j * cell_size_vert)), 2)
 
-            
+        def eventChecker():
+             for event in pygame.event.get():
+                  if event.type == pygame.QUIT:
+                       sys.exit()
+                  elif event.type == KEYDOWN and event.jey == k_q:
+                       pygame.quit()
+                       sys.exit()   
         def fillGrid():
-             pygame.draw.rect(map._VARS['surf'], map.BLACK, (18, 18, 60, 60))
+             
+             pygame.draw.rect(map._VARS['surf'], map.RED, (18, 18, 60, 60))
+             
 
+class cells:
+    row, column = 400, 400
+    position = [[0 for  x in range(row)] for y in range(column)]
 
 class pacrr:
     def init(self, world_size = 100.0, min_sensing_range = 0.3, max_sensing_range = 12, motion_noise = 1.0, sensor_noise = 1.0):
@@ -54,7 +66,7 @@ class pacrr:
          self.x = world_size / 2.0
          self.y = world_size / 2.0
          self.motion_noise = motion_noise
-         se
+         #se
 
 #tracking
 
@@ -63,7 +75,14 @@ class pacrr:
 #map optimization
 
 def main():
+    for i in range(cells.row):
+        for j in range(cells.column):
+            position[i][j] = 0 #initalizes grid to zero
+    cells.position[row/2][column/2] = 1 #initializes robot to be in center of grid
     pygame.init()
     map._VARS['surf'] = pygame.display.set_mode(map.GRIDSIZE)
+   
+    
+
     #NEED TO FINISH THIS PART
     
