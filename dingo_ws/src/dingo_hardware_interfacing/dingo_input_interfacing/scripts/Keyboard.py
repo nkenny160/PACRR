bafb95d189d581b585782e9db852441f4806d0bc
nkenny160 <nkenny@scu.edu>
2023-12-08 13:41:51 -0800

    keyboard control via ssh

--- dingo_ws/src/dingo_hardware_interfacing/dingo_input_interfacing/scripts/Keyboard.py
+++ dingo_ws/src/dingo_hardware_interfacing/dingo_input_interfacing/scripts/Keyboard.py
@@ -1,32 +1,37 @@
 import rospy
 import sys, signal
 import os
-from pynput import keyboard
 from sensor_msgs.msg import Joy
+import threading
+import time
+from sshkeyboard import listen_keyboard, stop_listening
+
 
 class Keyboard:
     def __init__(self):
-        self.used_keys = ['w','a','s','d','1','2', '7','8','9','0', keyboard.Key.shift, keyboard.Key.backspace, keyboard.Key.up, keyboard.Key.down, keyboard.Key.left, keyboard.Key.right]
+        self.used_keys = ['w', 'a', 's', 'd', '1', '2', '7', '8', '9', '0', "backspace", "up", "down", "left", "right"]
         self.speed_multiplier = 1
         self.joystick_message_pub = rospy.Publisher("joy", Joy, queue_size=10)
-        self.keyboard_listener = keyboard.Listener(
-            on_press=self.on_press,
-            on_release=self.on_release)
-        self.keyboard_listener.start()
-
         self.current_joy_message = Joy()
-        self.current_joy_message.axes = [0.,0.,0.,0.,0.,0.,0.,0.]
-        self.current_joy_message.buttons = [0,0,0,0,0,0,0,0,0,0,0]
+        self.current_joy_message.axes = [0., 0., 0., 0., 0., 0., 0., 0.]
+        self.current_joy_message.buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.start_keyboard_listener()
 
-        
+    def start_keyboard_listener(self):
+        keyboard_thread = threading.Thread(target=self.start_listen_keyboard)
+        keyboard_thread.daemon = True
+        keyboard_thread.start()
+
+    def start_listen_keyboard(self):
+        listen_keyboard(on_press=self.on_press, on_release=self.on_release)
+
     def on_press(self,key):
-        if hasattr(key, 'char'):
-            key = key.char
-        msg = self.current_joy_message
 
-        if key == keyboard.Key.shift:
-            self.speed_multiplier = 2
-        elif key == 'w' or key == 'W':
+        msg = self.current_joy_message
+        print(key)
+
+
+        if key == 'w' or key == 'W':
             msg.axes[1] = 0.5*self.speed_multiplier
         elif key == 's' or key == 'S':
             msg.axes[1] = -0.5*self.speed_multiplier
@@ -38,15 +43,15 @@ class Keyboard:
             msg.buttons[5] = 1
         elif key == '2':
             msg.buttons[0] = 1
-        elif key == keyboard.Key.backspace:
+        elif key == "backspace":
             msg.buttons[4] = 1
-        elif key == keyboard.Key.up:
+        elif key == "up":
             msg.axes[4] = 0.5*self.speed_multiplier
-        elif key == keyboard.Key.down:
+        elif key == "down":
             msg.axes[4] = -0.5*self.speed_multiplier
-        elif key == keyboard.Key.left:
+        elif key == "left":
             msg.axes[3] = 0.5*self.speed_multiplier
-        elif key == keyboard.Key.right:
+        elif key == "right":
             msg.axes[3] = -0.5*self.speed_multiplier
         elif key == '0':
             msg.axes[7] = 1
@@ -61,14 +66,12 @@ class Keyboard:
         return
 
     def on_release(self, key):
-        if hasattr(key, 'char'):
-            key = key.char
+
 
         msg = self.current_joy_message
 
-        if key == keyboard.Key.shift:
-            self.speed_multiplier = 1
-        elif key == 'w' or key == 'W':
+
+        if key == 'w' or key == 'W':
             msg.axes[1] = 0.0
         elif key == 's' or key == 'S':
             msg.axes[1] = 0.0
@@ -80,15 +83,15 @@ class Keyboard:
             msg.buttons[5] = 0
         elif key == '2':
             msg.buttons[0] = 0
-        elif key == keyboard.Key.backspace:
+        elif key == "backspace":
             msg.buttons[4] = 0
-        elif key == keyboard.Key.up:
+        elif key == "up":
             msg.axes[4] = 0.0
-        elif key == keyboard.Key.down:
+        elif key == "down":
             msg.axes[4] = 0.0
-        elif key == keyboard.Key.left:
+        elif key == "left":
             msg.axes[3] = 0.0
-        elif key == keyboard.Key.right:
+        elif key == "right":
             msg.axes[3] = 0.0
         elif key == '0':
             msg.axes[7] = 0
@@ -101,8 +104,9 @@ class Keyboard:
 
         
         self.current_joy_message = msg
-    
+
     def publish_current_command(self):
+        
         self.current_joy_message.header.stamp = rospy.Time.now()
         self.joystick_message_pub.publish(self.current_joy_message)
 
@@ -110,8 +114,6 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 def main():
-    """Main program
-    """
     rospy.init_node("keyboard_input_listener")
     rate = rospy.Rate(30)
 
@@ -123,8 +125,19 @@ def main():
     signal.signal(signal.SIGINT, signal_handler)
     keyboard_listener = Keyboard()
 
-    while not rospy.is_shutdown():
-        keyboard_listener.publish_current_command()
-        rate.sleep()
+    try:
+        while not rospy.is_shutdown():
+            keyboard_listener.publish_current_command()
+            time.sleep(1)
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Keyboard node interrupted.")
+    except KeyboardInterrupt:
+        rospy.loginfo("Keyboard node stopped by user.")
+    finally:
+        rospy.loginfo("Exiting...")
+        stop_listening()
+        sys.exit(0)
 
-main()
+
+if __name__ == "__main__":
+    main()
